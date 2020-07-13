b7c217ab6eb2833e5484f35805f89a08eeebe5c3
Kevin <kmadden84@gmail.com>
2020-07-13 13:38:42 -0400

    Update functions.js

--- functions/functions.js
+++ functions/functions.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 
 const apiKey = process.env.REACT_APP_APIKey;
 
-app.get('/topheadlines', (req, res) => {
+app.post('/topheadlines', (req, res) => {
   var pageNum = req.body.pageNum;
   const API_ENDPOINT = `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?country=us&pageSize=20&page=${pageNum}&apiKey=${apiKey}`
   return fetch(API_ENDPOINT, {
@@ -17,7 +17,7 @@ app.get('/topheadlines', (req, res) => {
     .catch(error => ({ statusCode: 422, body: String(error) }));
   });
 
-app.get('/query', (req, res) => {
+app.post('/query', (req, res) => {
   var query = req.body.query;
   var page = req.body.page;
   const API_ENDPOINT = `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?q=${query}&pageSize=20&page=${page}&apiKey=${apiKey}`
@@ -35,7 +35,7 @@ app.get('/query', (req, res) => {
   });
 
 
-  app.get('/noquery', (req, res) => {
+  app.post('/noquery', (req, res) => {
     var page = req.body.page;
     const API_ENDPOINT = `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?country=us&pageSize=20&page=${page}&apiKey=${apiKey}`
     return fetch(API_ENDPOINT, {
